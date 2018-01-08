40b38b60583e84633513bde8f8aecc9c3febb1b9
MichaLasry <michalasry10@gmail.com>
2018-01-08 18:08:38 +0200

    Green code 2

--- cash.js
+++ cash.js
@@ -1,1 +1,7 @@
 var coins = [5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
+var C = {};                    // C Object simplifies exporting the module
+C.getChange = function () {    // enough to satisfy the test
+    'use strict';
+    return true;               // also passes JSLint
+};
+module.exports = C;            // export the module with a single method
